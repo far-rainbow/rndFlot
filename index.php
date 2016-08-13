18228ae4045ae863dd4727cbcb7f2a98c185647e
root <root@debian-512mb-lon1-01>
2016-08-13 21:45:31 +0300

    Init commit =)

---
+++ index.php
@@ -0,0 +1,139 @@
+<html>
+<head>
+<title>Fast Site</title>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<link href="css/style.css" rel="stylesheet" />
+</head>
+
+<body>
+
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+<div class="container-fluid">
+
+<div class="navbar-header">
+<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#shapka">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span> 
+</button>
+<p class="navbar-brand">Генератор v 0.7</p>
+</div>
+<div class="collapse navbar-collapse" id="shapka">
+<ul class="nav navbar-nav">
+  <li class="dropdown" id="btn1">
+  <a class="dropdown-toggle" data-toggle="dropdown" href="#">Генератор
+  <span class="caret"></span>
+  <ul class="dropdown-menu" role="menu">
+    <li><a href="#lowSize">Слабо</a></li>
+    <li><a href="#midSize">Нормально так</a></li>
+    <li><a href="#fullSize">На полную</a></li>
+    <li class="divider"></li>
+    <li><a href="#startStop">Стоп</a></li>
+	<li class="divider"></li>
+    <li class="disabled"><a href="#quit">QUIT</a></li>
+  </ul>
+  </li>
+
+  <li class="dropdown" id="btn2">
+  <a class="dropdown-toggle" data-toggle="dropdown" href="#">Переход
+  <span class="caret"></span>
+  <ul class="dropdown-menu" role="menu">
+    <li><a href="#yandex">Yandex</a></li>
+    <li><a href="#google">Google</a></li>
+    <li><a href="#kamenka">Kamenka</a></li>
+  </ul>  
+  </li>
+</ul>
+<ul class="nav navbar-nav navbar-right" style="margin-top: -10px;">
+<li>
+<!-- Yandex.Metrika informer --> 
+<a href="https://metrika.yandex.ru/stat/?id=39027345&amp;from=informer"
+target="_blank" rel="nofollow"><img src="https://informer.yandex.ru/informer/39027345/3_1_FFFFFFFF_EFEFEFFF_0_pageviews"
+style="width:88px; height:31px; border:0;" alt="Яндекс.Метрика" title="Яндекс.Метрика: данные за сегодня (просмотры, визиты и уникальные посетители)" class="ym-advanced-informer" data-cid="39027345" data-lang="ru" /></a>
+<!-- /Yandex.Metrika informer -->
+</li>
+</ul>
+
+<!--
+<div class="btn-group">
+<div id="clock" class="light">
+			<div class="display">
+				<div class="weekdays"></div>
+				<div class="ampm"></div>
+				<div class="digits"></div>
+			</div>
+</div>
+</div>
+-->
+</div>
+</nav>
+
+<div class="container ">
+<table class="table" id="tableLines">
+<?php
+for ($i = 1; ; $i++) {
+echo <<< xxx
+<div class="pull-left" style="width: 15px; font-size: 6pt;">Г.$i</div><div class="progress">
+<div id="pbar_$i" class="progress-bar" role="progressbar" aria-valuenow="$i" aria-valuemin="0" aria-valuemax="100" style="width: $i%;">$i%</div></div>
+xxx;
+		if ($i == 8) break;
+}
+?>
+</table>
+</div> <!-- container -->
+<div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-md-7 col-sm-6">
+                    <div class="panel panel-default drop-shadow" style="margin-top: -16px;">
+                        <div class="panel-heading">
+                            Сгенерированные случайные величины (max <= шир.ген. в пикс.)
+                        </div>
+                        <!-- /.panel-heading -->
+                        <div class="panel-body">
+                            <div class="flot-chart">
+                                <div class="flot-chart-content" id="flot-line-chart-multi"></div>
+                            </div>
+                        </div>
+                        <!-- /.panel-body -->
+                    </div>
+                    <!-- /.panel -->
+                </div>
+				<div class="col-lg-4 col-md-4 col-sm-6">
+					<div class="panel panel-default drop-shadow" style="margin-top: -16px;">
+                        <div class="panel-heading">
+                            Круговая диаграмма сумм случайных чисел каждого генератора
+                        </div>
+                        <!-- /.panel-heading -->
+                        <div class="panel-body">
+                            <div class="flot-pie">
+                                <div class="flot-pie-content" id="flot-pie"></div>
+                            </div>
+                        </div>
+                        <!-- /.panel-body -->
+                    </div>
+                    <!-- /.panel -->
+				</div>
+            </div>
+            <!-- /.row -->
+</div>
+<div class="container">
+<p id="iteration" class="navbar-text" style="margin-top: -16px">-----</p>
+</div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<script src="http://cdnjs.cloudflare.com/ajax/libs/moment.js/2.0.0/moment.min.js"></script>
+<!-- <script src="js/script.js"></script> -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/flot/0.8.3/jquery.flot.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/flot/0.8.3/jquery.flot.pie.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/flot/0.8.3/jquery.flot.resize.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/flot/0.8.3/jquery.flot.time.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/flot/0.8.3/jquery.flot.categories.min.js"></script> 
+<script src="js/js.js"></script>
+<?include "js/ya.metrika.js"?>
+</body>
+</html>
---
+++ js/js.js
@@ -0,0 +1,131 @@
+btnVal = 0;
+btnValArr = {'#lowSize': 0.1,'#midSize': 0.5,'#fullSize': 1.0,'#startStop': false};
+modSize = 0;
+iteration = 0;
+query = [];
+total = [ 
+{label: 0, data: 0},
+{label: 1, data: 0},
+{label: 2, data: 0},
+{label: 3, data: 0},
+{label: 4, data: 0},
+{label: 5, data: 0},
+{label: 6, data: 0},
+{label: 7, data: 0},
+{label: 8, data: 0},
+{label: 9, data: 0},
+{label: 10, data: 0},
+{label: 11, data: 0},
+{label: 12, data: 0},
+{label: 13, data: 0},
+{label: 14, data: 0},
+{label: 15, data: 0},
+{label: 16, data: 0},
+];
+
+options = {
+		series: {
+			bars: {
+				show:true
+			}
+		},
+		bars: {
+			barWidth: 0.75
+		},
+		xaxis: {
+			mode: "categories"
+		},
+		yaxis: {
+			min: 0,
+			max: $('#tableLines').width()
+		}
+};
+
+pie_options = {
+		series: {
+			pie: {
+				show:true,
+				radius: 1,
+				label: {
+					show: true,
+					radius: 3/4,
+					background: { 
+						opacity: 0.5,
+						color: '#000'
+					},
+					formatter: function (label, series) {                
+                return '<div style="border:1px solid grey;font-size:8pt;text-align:center;padding:5px;color:white;">' +
+                label + ' : ' +
+                Math.round(series.percent) + '% : ' +
+				total[label].data +
+                '</div>';
+            },
+
+				}
+			}
+		}
+};
+
+$('#btn1 .dropdown-menu li a').click(function(){
+	modSize = btnValArr[$(this).attr('href')];
+});
+
+$('#btn2 .dropdown-menu li a').click(function(){
+	query.shift();
+	switch ($(this).attr('href')) {
+		case '#yandex':
+			setTimeout(function() { window.location.href = 'https://yandex.ru/search/?lr=382&text='+query.join('%20'); }, 1000);
+			break;
+		case '#google':
+			setTimeout(function() { window.location.href = 'https://google.com/search?q='+query.join('%20'); }, 1000);
+			break;
+		case '#kamenka':
+			setTimeout(function() { window.location.href = 'http://02511.kamenka.su/node-js-top-stat'; }, 1000);
+			break;
+	}
+});
+
+! function anim() {
+var d = [];
+options.yaxis.max = $('#tableLines').width();
+
+	if (modSize) {
+		iteration++;
+		for (i=1;i<=16;i++)
+		{
+			rndWidth = Math.round(Math.random()*$('#pbar_'+i).parent().width() * modSize);
+			query[i] = rndWidth;
+			d.push( {	label: i,
+						data: [
+								[i,rndWidth]
+						]
+					}
+			);
+			total[i].data += rndWidth;
+			$('#pbar_'+i).width(rndWidth);
+			$('#pbar_'+i).text(rndWidth);
+		}
+		$.plot('#flot-line-chart-multi', d, options );
+		$.plot('#flot-pie', total , pie_options );
+}
+
+$('#iteration').text('Итерация №'+iteration);
+setTimeout (anim, 1000);
+}();
+
+// ! function getPrices() {
+	// linkCBR = 'https://crossorigin.me/http://www.cbr.ru/scripts/XML_daily.asp';
+	// var request = new XMLHttpRequest();
+	// request.open("GET", linkCBR, true);
+	// request.send();
+	// var xml = request.responseXML;
+	// var valute = xml.getElementsByTagName("ID");
+	// for(var i = 0; i < valute.length; i++) {
+		// var user = valute[i];
+		// var names = user.getElementsByTagName("Name");
+		// for(var j = 0; j < names.length; j++) {
+			// console.log(names[j].childNodes[0].nodeValue);
+		// }
+	// }
+// setTimeout(getPrices,5000);
+// }();
